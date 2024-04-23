2229549ef5dc16675c71963ab002afda09d347e3
21pwbcs0832-WP-Fall2023 <sajjadafridi6913@gmail.com>
2024-04-23 22:02:21 +0500

    changed

--- src/loginform/LoginForm.jsx
+++ src/loginform/LoginForm.jsx
@@ -10,12 +10,7 @@ import './login.css';
         phone : ''
       });
       const InputEvent = (Event) => {
-    // console.log(Event.target.value);
-    // console.log(Event.target.name);
-
-    // const value = Event.target.value;
-    // const name = Event.target.name;
-
+  
     const {value , name } = Event.target;
 
     setFullname((preValue) =>{
@@ -24,42 +19,7 @@ import './login.css';
         ...preValue,
          [name] : value
       }
-      // if(name === 'fname'){
-      //   return {
-      //     fname: value,
-      //     lname: preValue.lname,
-      //     email: preValue.email,
-      //     phone: preValue.phone
-      //   }
-       
-      // }
-      // else if(name === 'lname'){
-      //   return {
-      //     fname: preValue.fname,
-      //     lname: value,
-      //     email: preValue.email,
-      //     phone: preValue.phone
-      //   }
-       
-      // }
-      // else if(name === 'email'){
-      //   return {
-      //     fname: preValue.fname,
-      //     lname: preValue.lname,
-      //     email: value,
-      //     phone: preValue.phone
-      //   }
-       
-      // }
-      // else if(name === 'phone'){
-      //   return {
-      //     fname: preValue.fname,
-      //     lname: preValue.lname,
-      //     email: preValue.email,
-      //     phone: value
-      //   }
-       
-      // };
+    
     });
   };
 
@@ -83,7 +43,7 @@ import './login.css';
      <p> {Fullname.phone} </p> 
      <input type="text" placeholder='Enter Your Name' name = "fname" onChange={InputEvent} value={Fullname.fname}/> <br/>
      <input type="text" placeholder='Enter Your Last' name = "lname" onChange={InputEvent} value={Fullname.lname}/><br/> 
-     <input type="email" placeholder='Enter Your Your' name = "email" onChange={InputEvent} value={Fullname.email}/><br/> 
+     <input type="email" placeholder='Enter Your Email' name = "email" onChange={InputEvent} value={Fullname.email}/><br/> 
      <input type="number" placeholder='Enter Your Mobile Number' name = "phone" onChange={InputEvent} value={Fullname.phone}/><br/> 
      <button type='Submit'> Submit Me 👍 </button>
      </form>
